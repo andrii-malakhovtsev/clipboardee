85f5c323de651ee6078a631cdeb985c4617f5acd
andrii-malakhovtsev <malakhovtsevandrii@gmail.com>
2026-02-26 23:16:20 -0500

    Update deployment instructions

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,13 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  kit: {
-    adapter: adapter({
-      fallback: '404.html' // Helps with routing on GH Pages
-    }),
-    paths: {
-      // This is important! Use your repo name here.
-      base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-    }
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			// Replace 'your-repo-name' with your actual GitHub repository name
+			base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+		}
+	}
 };
+
+export default config;
